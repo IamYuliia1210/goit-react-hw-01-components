2a6ae688716a60292fed15085aa2d3b90d8b6baa
IamYuliia1210 <puhovaya161996@gmail.com>
2023-08-22 21:56:36 +0100

    Statistics

---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,19 @@
+import { ListStats, Wrap, ItemListStats, Topic } from './Statistics.styled';
+
+export const Statistics = ({ title, stats }) => {
+  return (
+    <Wrap>
+      {title && <Topic>{title}</Topic>}
+      <ListStats>
+        {stats.map(item => {
+          return (
+            <ItemListStats key={item.id} color={item.percentage}>
+              <span className="label">{item.label}</span>
+              <span className="percentage">{item.percentage + '%'}</span>
+            </ItemListStats>
+          );
+        })}
+      </ListStats>
+    </Wrap>
+  );
+};
---
+++ src/components/Statistics/Statistics.styled.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+export const Wrap = styled.section`
+  max-width: 450px;
+  margin: 0 auto;
+  padding: 50px 0;
+`;
+export const ListStats = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+`;
+export const ItemListStats = styled.li`
+  width: calc(100% / 3);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 5px;
+  color: #fff;
+  padding: 12px 0;
+  background-color: ${props => {
+    return `#${Math.floor((Math.random() * 1677215) / props.color)
+      .toString(10)
+      .padStart(6, 0)}`;
+  }};
+`;
+export const Topic = styled.h2`
+  text-align: center;
+  text-transform: uppercase;
+  font-size: 30px;
+  font-weight: 400;
+  color: #808080;
+  padding: 20px 0;
+  background-color: #fff;
+  margin-bottom: 0;
+`;
